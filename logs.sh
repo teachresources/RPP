67f6661a065a6a746636de1ff8ebc6b743ef1bbd
dcassol <danicassol@gmail.com>
2020-04-14 15:06:20 -0700

    14/04/2020 15:06:04

--- logs.sh
+++ logs.sh
@@ -4,5 +4,5 @@ dt=$(date '+%d/%m/%Y %H:%M:%S')
 git pull
 git add -A :/
 git commit -m "$dt" 
-git push -u origin master
-echo "Committed/pushed changes to master branch on GitHub"
+git push 
+echo "Committed/pushed changes to  GitHub"
